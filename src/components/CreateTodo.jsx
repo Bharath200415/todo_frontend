ae536a25fc4957fdeba0a637437f881ba3668042
Bharath200415 <bharath200415@gmail.com>
2025-11-18 23:45:43 +0530

    initial frontend files

---
+++ src/components/CreateTodo.jsx
@@ -0,0 +1,10 @@
+
+export function CreateTodo(){
+    return <div>
+        <input type="text" placeholder = "title"></input>
+        <input type="text" placeholdr="description"></input>
+
+        <button>Add todo</button>
+    </div>
+}
+
