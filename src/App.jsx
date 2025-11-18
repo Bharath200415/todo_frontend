058333bb29640312c587be6b81d057a0710dc218
Bharath200415 <bharath200415@gmail.com>
2025-11-19 01:09:25 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import { CreateTodo } from './components/CreateTodo'
 import { Todos } from './components/Todos'
 
 function App() {
-
+  
+  //we can deploy this frontend using vercel
+  //backend using render
   const [todos, setTodos] = useState([]);
 
   fetch("http://localhost:3000/todos")
